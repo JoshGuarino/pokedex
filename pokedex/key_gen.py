11c5934cc80cdddf0dbb6969fadb817a37848371
JoshGuarino <guar0638@fredonia.edu>
2019-05-12 15:59:24 -0400

    inital flask setup test

--- pokedex/key_gen.py
+++ pokedex/key_gen.py
@@ -6,9 +6,10 @@ def generate_key():
     spec_char = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '<', '>', '?', '/']
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
     alphabet_upper = []
+
     for i in range(len(alphabet)):
         alphabet_upper.append(alphabet[i].upper())
-
+        
     for j in range(key_len):
         switch = random.randint(1, 4)
         if switch == 1:
